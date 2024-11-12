d6f28297abee94ce8df6098d7e55f45d8b52d627
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-12 16:33:19 +0700

    feat : add speech provider and routes to Recipe screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipe/screen/recipe_screen.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_to_text_provider.dart';
+import 'model/meal.dart';
 import 'screen/search_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final SpeechToText speech = SpeechToText();
+  late SpeechToTextProvider speechProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    speechProvider = SpeechToTextProvider(speech);
+    _initSpeechState();
+  }
+
+  Future<void> _initSpeechState() async {
+    await speechProvider.initialize();
+  }
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
+    return ChangeNotifierProvider<SpeechToTextProvider>.value(
+      value: speechProvider,
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const SearchScreen(),
+        routes: {
+          '/recipe': (context){
+            final Meal meal = ModalRoute.of(context)!.settings.arguments as Meal;
+            return RecipeScreen(meal: meal);
+          },
+        },
       ),
-      home: const SearchScreen(),
     );
   }
 }
