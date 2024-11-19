0de53684f265941ae2a88bcc5f572277f9524251
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-19 09:05:23 +0700

    feat : add MealProvider and add isFavourite to Meal

--- lib/model/meal.dart
+++ lib/model/meal.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 class Meal {
   final String idMeal;
   final String strMeal;
@@ -6,6 +7,7 @@ class Meal {
   final String? strInstructions;
   final String? strYoutube;
   final String? strMealThumb;
+  bool isFavourite = false;
   final List<String> ingredients;
   final List<String> measures;
 
@@ -45,5 +47,24 @@ class Meal {
       ingredients: ingredients,
       measures: measures,
     );
+  }
+}
+
+class MealProvider with ChangeNotifier {
+  List<Meal> _meals = [];
+
+  List<Meal> get meals => _meals;
+
+  void setMeals(List<Meal> meals) {
+    _meals = meals;
+    notifyListeners();
+  }
+
+  void toggleFavourite(String idMeal) {
+    final mealIndex = _meals.indexWhere((meal) => meal.idMeal == idMeal);
+    if (mealIndex != -1) {
+      _meals[mealIndex].isFavourite = !_meals[mealIndex].isFavourite;
+      notifyListeners();
+    }
   }
 }
