597afeef28cb36784548fee486693f99e1f874ab
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-19 09:06:14 +0700

    feat : add favourite screen

---
+++ lib/screen/favourite_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipe/model/meal.dart';
+import 'package:recipe/screen/recipe_screen.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Favourite Recipes'),
+        backgroundColor: Colors.white,
+      ),
+      body: Consumer<MealProvider>(
+        builder: (context, mealProvider, child) {
+          final favouriteMeals = mealProvider.meals.where((meal) => meal.isFavourite).toList();
+          if(favouriteMeals.isEmpty) {
+            return const Center(child: Text('No favourite meals yet!'));
+          }else{
+            return ListView.builder(
+              itemCount: favouriteMeals.length,
+              itemBuilder: (context, index) {
+                final meal = favouriteMeals[index];
+                return ListTile(
+                  leading: Image.network(meal.strMealThumb ?? ''),
+                  title: Text(meal.strMeal),
+                  subtitle: Text(meal.strCategory ?? ''),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>RecipeScreen(meal: meal)));},
+                  trailing: GestureDetector(
+                    onTap: () {
+                      mealProvider.toggleFavourite(meal.idMeal);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(meal.isFavourite ? 'Add to favourite recipes' : 'Remove from favourite recipes'),
+                          duration: const Duration(milliseconds: 400),
+                        ),
+                      );
+                    },
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      height: 40,
+                      width: 40,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Icon(
+                          meal.isFavourite ? Icons.favorite : Icons.favorite_outline,
+                          color: meal.isFavourite ? Colors.redAccent : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        }
+      ),
+    );
+  }
+}
