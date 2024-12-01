1f3434a048980c602f63024fc85bbc538f295ea9
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-01 18:20:06 +0700

    feat : add provider and firebase to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:recipe/screen/main_screen.dart';
+import 'package:recipe/providers/comments_provider.dart';
+import 'package:recipe/screens/main_screen.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:speech_to_text/speech_to_text_provider.dart';
-import 'model/meal.dart';
+import 'firebase_options.dart';
+import 'providers/meal_provider.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -34,7 +42,8 @@ class _MyAppState extends State<MyApp> {
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context)=>MealProvider()),
-          ChangeNotifierProvider<SpeechToTextProvider>.value(value: speechProvider,)
+          ChangeNotifierProvider<SpeechToTextProvider>.value(value: speechProvider,),
+          ChangeNotifierProvider(create: (_) => CommentProvider()),
         ],
       child: MaterialApp(
         theme: ThemeData(
