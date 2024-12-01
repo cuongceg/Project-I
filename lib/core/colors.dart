832c47abfe21181e6e00cfb3bcf81335d58457fd
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-01 18:18:05 +0700

    refactor : add const colors for app

---
+++ lib/core/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ConstColor{
+  final primary = const Color(0xFF329932);
+  final onPrimary = const Color(0xFFFFFFFF);
+  final tertiary = const Color(0xFFcccccc);
+}
