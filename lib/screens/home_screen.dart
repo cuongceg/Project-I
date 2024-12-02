4ae089a32d0ae75e63397845fb6303d69a1ee4e3
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-03 00:05:25 +0700

    feat : add food news

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe/core/colors.dart';
 import 'package:recipe/core/fonts.dart';
+import 'package:recipe/model/food_news.dart';
 import 'package:recipe/screens/recipe_screen.dart';
 import 'package:recipe/screens/search_screen.dart';
 import 'package:recipe/screens/widgets/base_component.dart';
@@ -19,15 +20,18 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   ApiService apiService = ApiService();
   late Future<List<Meal>> _mealsFuture;
+  late Future<FoodNews> _foodNews;
 
   @override
   void initState() {
     super.initState();
-    _mealsFuture = apiService.fetchMeals(''); // Initial load with no query
+    _mealsFuture = apiService.fetchMeals('');
+    _foodNews = apiService.fetchFoodNews();
   }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     final mealProvider = Provider.of<MealProvider>(context);
     return Scaffold(
       backgroundColor: Colors.white,
@@ -71,6 +75,60 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
           _headingLine('Recipes of the week'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset('assets/images/steak.jpg',
+                width: width-30,
+                height: 200,
+                fit: BoxFit.fitWidth,),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Text(
+              "Grilled Steak with sliced potatoes and tomato",
+              style: ConstFonts().copyWith(fontWeight: FontWeight.bold,fontSize: 16),
+              softWrap: true,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3,),
+          ),
+          const SizedBox(height: 5),
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Text(
+              "Main Course, Beef",
+              style: ConstFonts().copyWith(color: Colors.grey,fontSize: 14),
+              softWrap: true,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,),
+          ),
+          _headingLine('Cooking news of the week'),
+          FutureBuilder(
+              future: _foodNews,
+              builder: (context,snapshot){
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: BaseComponent().loadingCircle());
+                } else if (snapshot.hasError || !snapshot.hasData || snapshot.data!.articles.isEmpty) {
+                  return const Center(child: Text('No news found.'));
+                } else {
+                  final List<Article> articles = snapshot.data!.articles;
+                  return Column(
+                    children: [
+                      for(int i = 0; i < 3; i++)
+                        _newsColumn(
+                          articles[i].urlToImage,
+                          articles[i].title,
+                          articles[i].description,
+                          width -30,
+                          articles[i].url
+                        ),
+                    ],
+                  );
+                }
+              }
+          )
         ],
       ),
     );
@@ -128,6 +186,53 @@ class _HomeScreenState extends State<HomeScreen> {
                 softWrap: true,
                 overflow: TextOverflow.ellipsis,
                 maxLines: 1,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _newsColumn(String? urlImage,String? title,String? description,double width,String url){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GestureDetector(
+        onTap: (){
+          apiService.openYoutubeVideo(url);
+        },
+        child: SizedBox(
+          width: width,
+          height: 310,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.network(urlImage ?? '',
+                  width: width,
+                  height: 200,
+                  fit: BoxFit.fitWidth,),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  title ?? '',
+                  style: ConstFonts().copyWith(fontWeight: FontWeight.bold,fontSize: 18),
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,),
+              ),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  description ?? '',
+                  style: ConstFonts().copyWith(color: Colors.grey,fontSize: 14),
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,),
+              ),
             ],
           ),
         ),
