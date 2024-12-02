1c18fbae16dd53e1b21396f71319218190c04bcf
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-03 00:11:29 +0700

    feat : get food news data

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,5 @@
+import 'package:recipe/core/string.dart';
+import 'package:recipe/model/food_news.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../model/meal.dart';
 import 'dart:convert';
@@ -5,7 +7,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService{
   Future<List<Meal>> fetchMeals(String query) async {
-    final url = Uri.parse('https://www.themealdb.com/api/json/v1/1/search.php?s=$query'); // replace with actual endpoint
+    final url = Uri.parse('https://www.themealdb.com/api/json/v1/1/search.php?s=$query');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -25,6 +27,17 @@ class ApiService{
     } else {
       throw 'Could not launch $url';
     }
+  }
+  
+  Future<FoodNews> fetchFoodNews() async {
+    final url = Uri.parse("https://newsapi.org/v2/everything?q=food recipe&from=2024-11-02&sortBy=publishedAt&apiKey=${ConstString.newsAPI}");
+    final response = await http.get(url);
 
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      return FoodNews.fromJson(jsonData);
+    } else {
+      throw Exception('Failed to load food news');
+    }
   }
 }
