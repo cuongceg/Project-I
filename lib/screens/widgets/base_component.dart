9407c3ede3cbafc11cd30fbcfed56c6903221efa
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-19 11:11:47 +0700

    refactor : add size loading screen

--- lib/screens/widgets/base_component.dart
+++ lib/screens/widgets/base_component.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:recipe/core/colors.dart';
 
 class BaseComponent{
-  Widget loadingCircle(){
-    return CircularProgressIndicator(
-      color: ConstColor().primary,
-      strokeWidth: 5,
+  Widget loadingCircle({double size = 50}){
+    return SizedBox(
+      width: size,
+      height: size,
+      child: CircularProgressIndicator(
+        color: ConstColor().primary,
+        strokeWidth: 5,
+      ),
     );
   }
 }
