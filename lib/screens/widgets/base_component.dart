eff819e4be9b0ecbbb1d9eaca75c2b777ebdae57
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-26 16:37:20 +0700

    refactor : add continue button

--- lib/screens/widgets/base_component.dart
+++ lib/screens/widgets/base_component.dart
@@ -12,4 +12,33 @@ class BaseComponent{
       ),
     );
   }
+
+  Widget continueButton({required Function() onPressed, required String text}){
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: ConstColor().primary,
+        minimumSize: const Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(text,style: const TextStyle(fontSize: 18,color: Colors.white,),),
+    );
+  }
+
+  Widget signInButton({required Function() onPressed, required String text,required Widget icon}){
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        minimumSize: const Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      icon: icon,
+      label: Text(text,style: const TextStyle(fontSize: 14,color: Colors.black,),),
+    );
+  }
 }
