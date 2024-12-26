478c97c7720e864a02cf93fb7c8ca65684dc0366
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-26 16:38:10 +0700

    refactor : remove food recommendation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:recipe/core/colors.dart';
 import 'package:recipe/core/fonts.dart';
 import 'package:recipe/core/string.dart';
-import 'package:recipe/model/food_news.dart';
-import 'package:recipe/screens/recipe_screen.dart';
-import 'package:recipe/screens/search_screen.dart';
 import 'package:recipe/screens/widgets/base_component.dart';
-import '../model/meal.dart';
-import '../providers/meal_provider.dart';
+import '../model/food_news.dart';
 import '../services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -20,22 +15,19 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   ApiService apiService = ApiService();
-  late Future<List<Meal>> _mealsFuture;
   late Future<FoodNews> _foodNews;
 
   @override
   void initState() {
     super.initState();
-    _mealsFuture = apiService.fetchMeals('');
     _foodNews = apiService.fetchFoodNews();
   }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    final mealProvider = Provider.of<MealProvider>(context);
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: ConstColor().background,
       body: ListView(
         children: [
           const SizedBox(height: 20),
@@ -50,31 +42,31 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           const SizedBox(height: 20),
           _headingLine('Recommendations'),
-          FutureBuilder<List<Meal>>(
-            future: _mealsFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: BaseComponent().loadingCircle());
-              } else if (snapshot.hasError || !snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No meals found.'));
-              } else {
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  mealProvider.setMeals(snapshot.data!);
-                });
-                return SizedBox(
-                  height: 270,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: mealProvider.meals.length,
-                    itemBuilder: (context, index) {
-                      final meal = mealProvider.meals[index];
-                      return _recipeColumn(meal);
-                    },
-                  ),
-                );
-              }
-            },
-          ),
+          // FutureBuilder<List<Meal>>(
+          //   future: _mealsFuture,
+          //   builder: (context, snapshot) {
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return Center(child: BaseComponent().loadingCircle());
+          //     } else if (snapshot.hasError || !snapshot.hasData || snapshot.data!.isEmpty) {
+          //       return const Center(child: Text('No meals found.'));
+          //     } else {
+          //       WidgetsBinding.instance.addPostFrameCallback((_) {
+          //         mealProvider.setMeals(snapshot.data!);
+          //       });
+          //       return SizedBox(
+          //         height: 270,
+          //         child: ListView.builder(
+          //           scrollDirection: Axis.horizontal,
+          //           itemCount: mealProvider.meals.length,
+          //           itemBuilder: (context, index) {
+          //             final meal = mealProvider.meals[index];
+          //             return _recipeColumn(meal);
+          //           },
+          //         ),
+          //       );
+          //     }
+          //   },
+          // ),
           _headingLine('Recipes of the week'),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
@@ -143,60 +135,60 @@ class _HomeScreenState extends State<HomeScreen> {
     return Padding(
       padding: const EdgeInsets.only(left: 12,bottom: 10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Text(title,style: ConstFonts().copyWith(fontWeight: FontWeight.bold,fontSize: 20),),
-          TextButton(
-            onPressed: (){
-              Navigator.push(context,(MaterialPageRoute(builder: (context)=>const SearchScreen())));
-            },
-            child: Text('See All',style : ConstFonts().copyWith(color: ConstColor().primary,fontSize: 15,fontWeight: FontWeight.w700),),
-          ),
+          // TextButton(
+          //   onPressed: (){
+          //     Navigator.push(context,(MaterialPageRoute(builder: (context)=>const SearchScreen())));
+          //   },
+          //   child: Text('See All',style : ConstFonts().copyWith(color: ConstColor().primary,fontSize: 15,fontWeight: FontWeight.w700),),
+          // ),
         ],
       ),
     );
   }
 
-  Widget _recipeColumn(Meal meal){
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: GestureDetector(
-        onTap: (){
-          Navigator.push(context,MaterialPageRoute(builder: (context)=>RecipeScreen(meal: meal)));
-        },
-        child: SizedBox(
-          width: 150,
-          height: 270,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(meal.strMealThumb ?? '',
-                  width: 150,
-                  height: 200,
-                  fit: BoxFit.cover,),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                meal.strMeal,
-                style: ConstFonts().copyWith(fontWeight: FontWeight.bold,fontSize: 18),
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 3,),
-              const SizedBox(height: 5),
-              Text(
-                meal.strCategory ?? '',
-                style: ConstFonts().copyWith(color: Colors.grey,fontSize: 14),
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  // Widget _recipeColumn(Meal meal){
+  //   return Padding(
+  //     padding: const EdgeInsets.symmetric(horizontal: 15),
+  //     child: GestureDetector(
+  //       onTap: (){
+  //         Navigator.push(context,MaterialPageRoute(builder: (context)=>RecipeScreen(meal: meal)));
+  //       },
+  //       child: SizedBox(
+  //         width: 150,
+  //         height: 270,
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             ClipRRect(
+  //               borderRadius: BorderRadius.circular(20),
+  //               child: Image.network(meal.strMealThumb ?? '',
+  //                 width: 150,
+  //                 height: 200,
+  //                 fit: BoxFit.cover,),
+  //             ),
+  //             const SizedBox(height: 10),
+  //             Text(
+  //               meal.strMeal,
+  //               style: ConstFonts().copyWith(fontWeight: FontWeight.bold,fontSize: 18),
+  //               softWrap: true,
+  //               overflow: TextOverflow.ellipsis,
+  //               maxLines: 3,),
+  //             const SizedBox(height: 5),
+  //             Text(
+  //               meal.strCategory ?? '',
+  //               style: ConstFonts().copyWith(color: Colors.grey,fontSize: 14),
+  //               softWrap: true,
+  //               overflow: TextOverflow.ellipsis,
+  //               maxLines: 1,),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _newsColumn(String? urlImage,String? title,String? description,double width,String? url){
     return Padding(
