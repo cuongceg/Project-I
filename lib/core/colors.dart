02c9aff899021e56fbbaf3aeeb7c138659a0eb44
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-26 16:33:48 +0700

    refactor : change primary and background color

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ConstColor{
-  final primary = const Color(0xFF329932);
+  final primary = const Color(0xFF606c38);
   final onPrimary = const Color(0xFFFFFFFF);
   final tertiary = const Color(0xFFcccccc);
+  final background = const Color(0xFFF2F4E9);
 }
